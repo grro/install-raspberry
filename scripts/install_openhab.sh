5fd8cabcfffeb3f2d6933801efb133ed4c66cd87
gregor roth <gregor.roth@1und1.de>
2022-02-24 12:10:51 +0100

    scripts updated

--- scripts/install_openhab.sh
+++ scripts/install_openhab.sh
@@ -6,16 +6,17 @@ if [ -d "/etc/openhab" ]
 then
     echo "openhab is already installed"
 else
+    echo "installing iputils"
+    sudo apt-get -y install iputils-arping
+
     echo "installing openhab"
     sudo wget -qO - 'https://openhab.jfrog.io/artifactory/api/gpg/key/public' | sudo apt-key add -
     echo 'deb https://openhab.jfrog.io/artifactory/openhab-linuxpkg stable main' | sudo tee /etc/apt/sources.list.d/openhab.list
-    sudo apt-get update
     sudo apt-get install openhab -y
     sudo adduser openhab dialout
     sudo adduser openhab tty
     sudo usermod -a -G dialout openhab
     setcap 'cap_net_raw,cap_net_admin=+eip cap_net_bind_service=+ep' $(realpath /usr/bin/java)
-    sudo apt-get -y install iputils-arping
 fi
 
 
@@ -23,11 +24,13 @@ if [ -f "/etc/default/openhab.org" ]
 then
     echo "/etc/default/openhab is already extended"
 else
+    tmp_dir=$(mktemp -d -t installation-XXXXXXXXXX)
     echo "extending /etc/default/openhab"
-    cp /etc/default/openhab $tmp_dir/openhab.tmp
+    sudo cp /etc/default/openhab $tmp_dir/openhab.tmp
     echo 'EXTRA_JAVA_OPTS="-Dgnu.io.rxtx.SerialPorts=/dev/ttyUSB-zwave:/dev/ttyUSB-enocean"' >> $tmp_dir/openhab.tmp
     sudo mv /etc/default/openhab /etc/default/openhab.org
-    cp $tmp_dir/openhab.tmp /etc/default/openhab
+    sudo cp $tmp_dir/openhab.tmp /etc/default/openhab
+    sudo rm $tmp_dir
 fi
 
 sudo apt-get install openhab-addons -y
