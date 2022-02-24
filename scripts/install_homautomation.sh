0e1d547113b7eb7b75dc94ace3624662accedcea
gregor roth <gregor.roth@1und1.de>
2022-02-24 08:47:02 +0100

    scripts added

--- scripts/install_homautomation.sh
+++ scripts/install_homautomation.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+sudo apt-get update
 curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/install_python.sh | bash
 curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/install_zulu_java_11.sh | bash
--- scripts/install_python.sh
+++ scripts/install_python.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+echo "installing python pip"
 sudo apt install python3-pip -y
