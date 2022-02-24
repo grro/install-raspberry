648d3166aabc5351a618745a6eec1cb92b165bd9
gregor roth <gregor.roth@1und1.de>
2022-02-24 16:40:14 +0100

    scripts updated

--- scripts/homeautomation/install_openhab.sh
+++ scripts/homeautomation/install_openhab.sh
@@ -31,7 +31,7 @@ if [ -f "/etc/udev/rules.d/50-usb-serial.rules" ]
 then
     echo "/etc/udev/rules.d/50-usb-serial.rules is already created"
 else
-    echo "creating /etc/udev/rules.d/50-usb-serial.rules"
+    echo "create /etc/udev/rules.d/50-usb-serial.rules"
     echo 'SUBSYSTEM=="tty", ATTRS{idVendor}=="0658", ATTRS{idProduct}=="0200", SYMLINK+="ttyUSB-zwave", GROUP="dialout", MODE="0666"' >> 50-usb-serial.tmp
     echo 'SUBSYSTEM=="tty", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", SYMLINK+="ttyUSB-enocean", GROUP="dialout", MODE="0666"' >> 50-usb-serial.tmp
     sudo mv 50-usb-serial.tmp /etc/udev/rules.d/50-usb-serial.rules
