bb1973b8bcfd1a2ee0f6c5d20d115d9d03012495
gregor roth <gregor.roth@1und1.de>
2022-02-24 08:40:52 +0100

    scripts added

---
+++ scripts/install_homautomation.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/install_python.sh | bash
+curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/install_zulu_java_11.sh | bash
