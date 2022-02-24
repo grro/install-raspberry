a199bf610148dd6694082ad4fe2386443760d85a
gregor roth <gregor.roth@1und1.de>
2022-02-24 11:52:03 +0100

    scripts updated

--- scripts/install_homautomation.sh
+++ scripts/install_homautomation.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+
 ##################################################
 #
 # run this script by executing
 # sudo curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/install_homautomation.sh | bash
 #
 ###################################################
-
 
 
 #sudo apt-get update
@@ -19,4 +19,4 @@ sudo curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/sc
 
 # sudo curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/install_openhab.sh | bash
 
-# sudo curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/install_samba_for_openhab.sh | bash
+sudo curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/install_samba_for_openhab.sh | bash
