2b3f6cff0ea08fb57a1ee0ac947efa940816ae1e
gregor roth <gregor.roth@web.de>
2022-02-26 15:20:16 +0100

    scripts updated

--- scripts/homeautomation/install_chromium_kioskmode.sh
+++ scripts/homeautomation/install_chromium_kioskmode.sh
@@ -23,10 +23,12 @@ else
     echo '# Remove exit errors from the config files that could trigger a warning' >> autostart.tmp
     echo "sed -i 's/\"exited_cleanly\":false/\"exited_cleanly\":true/' ~/.config/chromium/\'Local State'" >> autostart.tmp
     echo "sed -i 's/\"exited_cleanly\":false/\"exited_cleanly\":true/; s/\"exit_type\":\"[^\"]\+\"/\"exit_type\":\"Normal\"/' ~/.config/chromium/Default/Preferences" >> autostart.tmp
-    echo '# Run Chromium in kiosk mode' >> autostart.tmp
-    echo 'chromium-browser  --noerrdialogs --check-for-update-interval=31536000 --disable-infobars --kiosk http://localhost:8080/habpanel/index.html# &' >> autostart.tmp
     sudo cp /etc/xdg/openbox/autostart /etc/xdg/openbox/autostart.org
     sudo cp autostart.tmp /etc/xdg/openbox/autostart
+
+    # add to chron
+    # chromium-browser  --noerrdialogs --check-for-update-interval=31536000 --disable-infobars --kiosk http://localhost:8080/habpanel/index.html# &
+
 fi
 
 
