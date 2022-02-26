183717eb775051ad91085697ea5155dd2838fa82
gregor roth <gregor.roth@web.de>
2022-02-26 15:13:33 +0100

    scripts updated

--- scripts/homeautomation/install_homautomation.sh
+++ scripts/homeautomation/install_homautomation.sh
@@ -10,16 +10,19 @@
 
 sudo curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/homeautomation/install_update.sh | bash
 
-# sudo apt-get install xrdp -y
 
-sudo curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/homeautomation/install_python.sh | bash
+# will be removed
+sudo apt-get install xrdp -y
 
-sudo curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/homeautomation/install_zulu_java_11.sh | bash
 
-sudo curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/homeautomation/install_mutt.sh | bash
+#sudo curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/homeautomation/install_python.sh | bash
 
-sudo curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/homeautomation/install_openhab.sh | bash
+#sudo curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/homeautomation/install_zulu_java_11.sh | bash
 
-sudo curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/homeautomation/install_samba_for_openhab.sh | bash
+#sudo curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/homeautomation/install_mutt.sh | bash
+
+#sudo curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/homeautomation/install_openhab.sh | bash
+
+#sudo curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/homeautomation/install_samba_for_openhab.sh | bash
 
 sudo curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/homeautomation/install_chromium_kioskmode.sh | bash
