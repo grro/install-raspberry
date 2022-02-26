8b3b4c4a75ff1331f03fb6ebc8519da5e88888fd
gregor roth <gregor.roth@web.de>
2022-02-26 15:47:12 +0100

    scripts updated

--- scripts/homeautomation/install_chromium_kioskmode.sh
+++ scripts/homeautomation/install_chromium_kioskmode.sh
@@ -15,15 +15,17 @@ else
     #sudo apt-get install chromium-browser -y
     #sudo apt-get install unclutter -y
 
-    cp /etc/xdg/openbox/autostart autostart.tmp
-    echo 'xset s noblank        # turn off screen blanking' >> autostart.tmp
-    echo 'xset s off            # turn off screen saver' >> autostart.tmp
-    echo '@unclutter' >> autostart.tmp
-    echo '# Remove exit errors from the config files that could trigger a warning' >> autostart.tmp
-    echo "sed -i 's/\"exited_cleanly\":false/\"exited_cleanly\":true/' ~/.config/chromium/\'Local State'" >> autostart.tmp
-    echo "sed -i 's/\"exited_cleanly\":false/\"exited_cleanly\":true/; s/\"exit_type\":\"[^\"]\+\"/\"exit_type\":\"Normal\"/' ~/.config/chromium/Default/Preferences" >> autostart.tmp
+    sudo cp /etc/xdg/openbox/autostart autostart.tmp
+    cp autostart.tmp autostart.tmp2
+    echo 'xset s noblank        # turn off screen blanking' >> autostart.tmp2
+    echo 'xset s off            # turn off screen saver' >> autostart.tmp2
+    echo '@unclutter' >> autostart.tmp2
+    echo '# Remove exit errors from the config files that could trigger a warning' >> autostart.tmp2
+    echo "sed -i 's/\"exited_cleanly\":false/\"exited_cleanly\":true/' ~/.config/chromium/\'Local State'" >> autostart.tmp2
+    echo "sed -i 's/\"exited_cleanly\":false/\"exited_cleanly\":true/; s/\"exit_type\":\"[^\"]\+\"/\"exit_type\":\"Normal\"/' ~/.config/chromium/Default/Preferences" >> autostart.tmp2
     sudo cp /etc/xdg/openbox/autostart /etc/xdg/openbox/autostart.org
-    sudo cp autostart.tmp
+    sudo cp autostart.tmp2 /etc/xdg/openbox/autostart
+    sudo rm autostart.tmp
 
     # add to chron
     # chromium-browser  --noerrdialogs --check-for-update-interval=31536000 --disable-infobars --kiosk http://localhost:8080/habpanel/index.html# &
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='install-raspberry',
+    version='',
+    packages=[''],
+    url='https://github.com/grro/httpstreamproxy',
+    license='Apache Software License',
+    author='grro',
+    author_email='gregor.roth@web.de',
+    description='test'
+)
