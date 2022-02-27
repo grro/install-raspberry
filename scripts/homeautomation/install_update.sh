e13d300829e1687280631b38e22617d29f5005e9
gregor roth <gregor.roth@web.de>
2022-02-27 09:11:43 +0100

    scripts updated

--- scripts/homeautomation/install_update.sh
+++ scripts/homeautomation/install_update.sh
@@ -11,7 +11,7 @@ else
     echo ""
     echo "++++++++++"
     echo "+ perform repo update "
-    sudo apt update && sudo upgrade -y
+    sudo apt update && sudo apt upgrade -y
     echo 'marker file' >> __installation_homeautomation_repo_updated_marker.tmp
     sudo mv __installation_homeautomation_repo_updated_marker.tmp /tmp/__installation_homeautomation_repo_updated_marker.tmp
 fi
