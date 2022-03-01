bfbf3b84705743560974cb9b4ee75c38a0da107b
gregor roth <gregor.roth@web.de>
2022-03-01 14:41:17 +0100

    scripts updated

--- scripts/homeautomation/install_chromium_kioskmode.sh
+++ scripts/homeautomation/install_chromium_kioskmode.sh
@@ -14,22 +14,20 @@ else
     echo "+ install unclutter "
     sudo apt-get install unclutter -y
 
-
     echo "+ update autostart "
     echo "#!/bin/bash" >> start_browser.sh.tmp
     echo "sleep 90" >> start_browser.sh.tmp
     echo "chromium-browser --kiosk --noerrdialogs  --check-for-update-interval=31536000 --disable-infobars http://0.0.0.0:8080/habpanel/index.html#/view/home" >> start_browser.sh.tmp
-    sudo mkdir /etc/kioskbrowser
-    sudo mv start_browser.sh.tmp /etc/kioskbrowser/start_browser.sh
-    sudo chmod +x /etc/kioskbrowser/start_browser.sh
-
+    sudo mkdir /etc/openhab/exec
+    sudo mv start_browser.sh.tmp /etc/openhab/exec/start_browser.sh
+    sudo chmod +x /etc/openhab/exec/start_browser.sh
 
     echo '  ' >> autostart_extension.tmp
     echo '# disable curser' >> autostart_extension.tmp
     echo '@unclutter' >> autostart_extension.tmp
     echo '  ' >> autostart_extension.tmp
     echo '# start Chromium in full mode' >> autostart_extension.tmp
-    echo '@/etc/kioskbrowser/start_browser.sh' >> autostart_extension.tmp
+    echo '@/etc/openhab/exec/start_browser.sh' >> autostart_extension.tmp
     sudo cp /etc/xdg/lxsession/LXDE-pi/autostart autostart.org
     cat autostart.org >> autostart.extended
     cat autostart_extension.tmp >> autostart.extended
