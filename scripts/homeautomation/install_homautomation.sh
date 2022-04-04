6b700cbb82f67044a818da09bee0543d30b56a6a
gregor roth <gregor.roth@web.de>
2022-04-04 09:17:20 +0200

    scripts updated

--- scripts/homeautomation/install_homautomation.sh
+++ scripts/homeautomation/install_homautomation.sh
@@ -19,10 +19,10 @@ sudo curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/sc
 
 sudo curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/homeautomation/modules/install_mutt.sh | bash
 
-sudo curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/homeautomation/modules/install_openhab.sh | bash
-
 sudo curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/homeautomation/modules/install_samba_for_openhab.sh | bash
 
-#sudo curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/homeautomation/modules/install_chromium_kioskmode.sh | bash
+sudo curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/homeautomation/modules/install_openhab.sh | bash
+
+# sudo curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/homeautomation/modules/install_frontail.sh | bash
 
 #sudo apt autoremove
---
+++ scripts/homeautomation/install_nas.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+##################################################
+#
+# run this script by executing
+# sudo curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/homeautomation/install_nas.sh | bash
+#
+###################################################
+
+cd ~
+
+sudo curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/homeautomation/modules/install_update.sh | bash
+
+sudo curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/homeautomation/modules/install_python.sh | bash
+
+sudo curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/homeautomation/modules/install_mutt.sh | bash
+
+sudo curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/homeautomation/modules/install_samba.sh | bash
+
+echo 'run sudo smbpasswd -a <user>'
+
+#sudo apt autoremove
