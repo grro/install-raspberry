e54893fd90cc938648542588a2bd2d552daf35b2
gregor roth <gregor.roth@web.de>
2022-04-04 09:21:56 +0200

    scripts updated

---
+++ scripts/homeautomation/modules/install_unifi_controller.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+if [ -f "/tmp/__installation_homeautomation_repo_updated_marker.tmp" ]
+then
+    echo ""
+    echo "++++++++++"
+    echo "+ unifi controller is already installed"
+else
+    echo ""
+    echo "++++++++++"
+    echo "+ install unifi controller "
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv C0A52C50
+    echo 'deb http://www.ubnt.com/downloads/unifi/debian stable ubiquiti' | sudo tee -a /etc/apt/sources.list.d/ubnt.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install unifi -y
+fi
