319c6b11f9bf6d85ccc2bd8e8a4b12e3f3d216e0
gregor roth <gregor.roth@web.de>
2022-04-06 18:00:31 +0200

    scripts updated

--- scripts/homeautomation/bootstrap.sh
+++ scripts/homeautomation/bootstrap.sh
@@ -7,12 +7,13 @@
 #
 ###################################################
 
+sudo mkdir /etc/scripts
+sudo mkdir /etc/scripts/modules
 
 sudo curl -s -L  --output /etc/scripts/modules/install_samba.sh https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/homeautomation/install_samba.sh
 
-sudo mkdir /etc/scripts
-sudo mkdir /etc/scripts/modules
 sudo chmod +x /etc/scripts/modules/install_samba.sh
 sudo chmod -R 777 /etc/scripts/
+
 sudo /etc/scripts/modules/install_samba.sh
 
