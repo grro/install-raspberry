6068ce90262f89579813d6bfa463e046adacd656
gregor roth <gregor.roth@web.de>
2022-04-06 18:56:07 +0200

    scripts updated

--- scripts/homeautomation/install_samba.sh
+++ scripts/homeautomation/install_samba.sh
@@ -12,21 +12,7 @@ else
     echo "+ installing samba"
     sudo apt update && sudo apt upgrade -y
     sudo apt-get install samba samba-common smbclient -y
-fi
 
-
-if [ -f "/etc/samba/smb.conf.org" ]
-then
-    echo ""
-    echo "++++++++++"
-    echo "+ samba config already updated"
-else
-    echo ""
-    echo "++++++++++"
-    echo "+ updating samba config"
-    #sudo apt update && sudo apt upgrade -y
-
-    sudo apt-get install samba samba-common smbclient -y
     sudo curl -o smb.tmp https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/homeautomation/smb.conf
     sudo cp /etc/samba/smb.conf /etc/samba/smb.conf.org
     sudo mv smb.tmp /etc/samba/smb.conf
