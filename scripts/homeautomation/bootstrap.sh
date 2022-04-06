420797e027d12fda4c569626d3b4752711d78779
gregor roth <gregor.roth@web.de>
2022-04-06 10:04:32 +0200

    scripts updated

---
+++ scripts/homeautomation/bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+##################################################
+#
+# run this script by executing
+# sudo curl -s -L https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/homeautomation/bootstrap.sh | bash
+#
+###################################################
+
+sudo mkdir /etc/scripts
+sudo mkdir /etc/scripts/modules
+
+sudo curl -s -L  --output /etc/scripts/modules/install_samba.sh https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/homeautomation/modules/install_samba.sh
+sudo chmod +x /etc/scripts/modules/install_samba.sh
+sudo /etc/scripts/modules/install_samba.sh
---
+++ scripts/homeautomation/install_samba.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+if [ -d "/etc/samba" ]
+then
+    echo ""
+    echo "++++++++++"
+    echo "+ samba is already installed"
+else
+    echo ""
+    echo "++++++++++"
+    echo "+ installing samba"
+    sudo apt-get install samba samba-common smbclient -y
+fi
+
+
+if [ -f "/etc/samba/smb.conf.org" ]
+then
+    echo ""
+    echo "++++++++++"
+    echo "+ samba config already updated"
+else
+    echo ""
+    echo "++++++++++"
+    echo "+ updating samba config"
+    sudo curl -o smb.tmp https://raw.githubusercontent.com/grro/install-raspberry/main/scripts/homeautomation/smb.conf
+    sudo cp /etc/samba/smb.conf /etc/samba/smb.conf.org
+    sudo mv smb.tmp /etc/samba/smb.conf
+
+    sudo chown -R pi:pi /etc/scripts
+    sudo chmod -R 700 /etc/scripts
+    sudo systemctl restart smbd
+
+    sudo smbpasswd -a pi
+fi
